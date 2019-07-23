c4c9d560f04ba24b08f82fb865b27ae06cb5b0f7
Alena Safronava <alena_safronava@epam.com>
2019-07-23 10:48:50 +0300

    Index-structured tables are not imported properly

--- templates/disable_constraints.sql
+++ templates/disable_constraints.sql
@@ -29,7 +29,6 @@ begin
                    where c.owner = v_tbl(i)
                      and t.owner = v_tbl(i)
                      and t.table_name = c.table_name
-                     and t.iot_type is null
                      and t.temporary = 'N'
                    order by decode(c.constraint_type, 'R', 1, 'P', 2, 'U', 3, 4))
         loop
